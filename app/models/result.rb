39a40f9cc1fef992600464cd3424998148d0e902
bach.do <k-i-k-ichi@users.noreply.github.com>
2016-08-23 13:51:37 +0700

    ref task #72059 create-exam + do_lesson

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,25 @@
 class Result < ActiveRecord::Base
-  include PublicActivity::Common
+
   belongs_to :lesson
   belongs_to :word
   belongs_to :answers
+
+  query = "INNER JOIN answers ON results.word_id =
+    answers.word_id AND results.answer_id = answers.id"
+  scope :in_lesson, -> (lesson) {where lesson_id: lesson.id}
+  scope :correct, -> {joins(query).merge(Answer.correct)}
+
+  def correct_answer
+    answer = self.word.answers.find{|a| a.is_correct}
+  end
+
+  def is_correct_answer?
+    if self.answer_id
+      Answer.find_by(id: self.answer_id).is_correct?
+    end
+  end
+
+  def user_answer
+    Answer.find_by id: self.answer_id
+  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,42 @@
 class Word < ActiveRecord::Base
+
+  QUERRY_WORD_TRUE_LEARNT = "id in (SELECT DISTINCT ls.word_id FROM lesson_words ls
+    INNER JOIN lessons l ON l.id = ls.lesson_id
+    INNER JOIN word_answers wa ON ls.word_id = wa.word_id AND ls.word_answer_id = wa.id
+    WHERE wa.is_correct = 't' AND l.user_id = :user_id AND l.is_completed = 't')"
+
+  QUERRY_WORD_WRONG_LEARNT = "id in (SELECT DISTINCT ls.word_id FROM
+    lesson_words ls JOIN lessons l ON ls.lesson_id = l.id
+    WHERE l.user_id = :user_id AND l.is_completed = 't'
+    EXCEPT
+    SELECT DISTINCT ls.word_id FROM lesson_words ls
+    INNER JOIN lessons l ON l.id = ls.lesson_id
+    INNER JOIN word_answers wa ON ls.word_id = wa.word_id AND ls.word_answer_id = wa.id
+    WHERE wa.is_correct = 't' AND l.user_id = :user_id AND l.is_completed = 't')"
+
+  QUERRY_WORD_LEARNT = "id in (SELECT DISTINCT ls.word_id FROM lesson_words ls
+    INNER JOIN lessons l ON ls.lesson_id = l.id
+    WHERE l.user_id = :user_id AND l.is_completed = 't')"
+
+  QUERRY_WORD_NOT_YET = "id not in (SELECT DISTINCT ls.word_id FROM
+    lesson_words ls JOIN lessons l ON ls.lesson_id = l.id
+    WHERE l.user_id = :user_id AND l.is_completed = 't')"
+
+  scope :random, -> {order "RANDOM()"}
+  scope :in_category, -> category_id do
+    where category_id: category_id if category_id.present?
+  end
+
   belongs_to :category
   has_many :answers, dependent: :destroy
   has_many :results, dependent: :destroy
+
   validates :content, presence: true, uniqueness: true
 
   accepts_nested_attributes_for :answers, allow_destroy: true
+
+
+  def correct_answer
+    self.answers.correct.first
+  end
 end
